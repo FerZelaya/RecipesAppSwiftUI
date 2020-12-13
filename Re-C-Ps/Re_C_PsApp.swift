91c8575e904f04d6fbf81292c805e80a20addc1a
Alcides Zelaya <alcideszelaya@hotmail.com>
2020-12-13 13:35:59 -0600

    Initial Commit

---
+++ Re-C-Ps/Re_C_PsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_C_PsApp.swift
+//  Re-C-Ps
+//
+//  Created by Alcides Zelaya on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Re_C_PsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
